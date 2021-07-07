8c96585983a3f61f10404305679f1d7b3ce76ad2
David <artcoder@gmail.com>
2021-07-07 05:57:58 -0500

    Refractored variables. Added corrections for the start and finish dates. Removed some comments.

--- rotate.py
+++ rotate.py
@@ -16,12 +16,18 @@ import pandas as pd
 import pandas_ta as ta
 import yfinance as yf
 
-finish = datetime.datetime(2021, 7, 2)
+extra_days = 5  # extra days to try to download in case the start date is not a trading day
+finish = datetime.datetime(2021, 7, 6)
 start = finish - timedelta(days=289)
+print("start:", start, " finish: ", finish)
+
+# finish = datetime.today()
+# start = finish - timedelta(days=289)
+
 # 253 trading days in a year
 # 365.25 days in a year
 # 200 trading days to look at
-# how many calendar days?
+# How many calendar days?
 # 200 * 365.25 / 253 = 289 calendar days
 
 # You need to copy the S&P 500 companies into a CSV file
@@ -38,33 +44,36 @@ def load_stock_data():
 
     global stock_df
 
-    stocks = []
+    stock_list = []
 
     csvfile = open(symbols_filename, newline='')
     reader = csv.reader(csvfile)
-    
-    for row in reader:
-        stocks.append(row[0])
 
-    for s in stocks:
-    
+    for row in reader:
+        stock_list.append(row[0])
+
+    for stock_symbol in stock_list:
+
         # print the symbol which is being downloaded
-        print(str(stocks.index(s)) + str(':') + s)
-    
+        print(str(stock_list.index(stock_symbol)) + str(':') + stock_symbol)
+
         try:
             # download the stock prices
-            stock = []
-            stock = yf.download(s, start=start, end=finish, progress=False)
-        
+            stock_data = []
+            stock_data = yf.download(stock_symbol,
+                                     start=(start - timedelta(days=extra_days)),
+                                     end=(finish + timedelta(days=1)),
+                                     progress=False)
+
             # append the individual stock prices 
-            if len(stock) == 0:
+            if len(stock_data) == 0:
                 None
             else:
-                stock['Name'] = s
-                stock_df = stock_df.append(stock, sort=False)
+                stock_data['Name'] = stock_symbol
+                stock_df = stock_df.append(stock_data, sort=False)
         except Exception:
-            print("Could not download " + s)
-
+            print("Could not download " + stock_symbol)
+###
 
 pickle_file = open(pickle_filename, 'rb')
 stock_df = pickle.load(pickle_file)
@@ -78,43 +87,75 @@ stock_df = stock_df.reset_index()
 stock_df = stock_df.drop(columns=['Open', 'High', 'Low', 'Close'])
 stock_df = stock_df.set_index(['Name', 'Date'])
 stock_df = stock_df.sort_index()
-
-#print("length=", len(stock_df.loc["AAPL"]))
-#print(stock_df.iloc[0])
-#print(stock_df.iloc[251])
-#print(stock_df.loc["AAPL"].iloc[51])
-#print(stock_df.loc["AAPL"].iloc[251])
-#print(stock_df.loc["AAPL"])
+#print(stock_df)
 
 temp_df = stock_df.reset_index()
 stocks = temp_df['Name'].unique()
 # print(stocks)
+
+
+# Correct start and finish dates so they are trading days
+trading_days = stock_df.loc[stocks[0]].index
+
+start_day_range = pd.date_range(start - timedelta(days=extra_days),
+                                start).tolist()
+start_day_range.reverse()
+found_start_day = False
+
+for d in start_day_range:
+    if d in trading_days:
+        start = d
+        found_start_day = True
+
+if found_start_day == False:
+    print('Could not find a trading day for the start day.')
+
+
+finish_day_range = pd.date_range(finish - timedelta(days=extra_days),
+                                 finish).tolist()
+found_finish_day = False
+
+for d in finish_day_range:
+    if d in trading_days:
+        finish = d
+        found_finish_day = True
+
+if found_finish_day == False:
+    print('Could not find a trading day for the finish day.')
+
+print("start:", start, " finish: ", finish)
 
 ROC = {}
 RSI = {}
 for stock in stocks:
     # print(stock)
     try:
-        last_price = stock_df.loc[stock, finish - timedelta(days=1)].get("Adj Close")
-        first_price = stock_df.loc[stock, start].get("Adj Close")
-        ROC[stock] = round(((last_price - first_price) / first_price) * 100,2)
-
-        temp = ta.rsi(stock_df.loc[stock, :].get("Adj Close"), length=3)
-        RSI[stock] = temp[finish - timedelta(days=1)]
-
+        # last_price = stock_df.loc[stock, finish - timedelta(days=1)].get("Adj Close")
+        last_price = stock_df.loc[stock, finish].get("Adj Close")
     except:
-        print("Failed to get start or finish price for " + stock)
+        print("Failed to get finish price for " + stock)
         continue
 
+    try:
+        first_price = stock_df.loc[stock, start].get("Adj Close")
+    except:
+        print("Failed to get start price for " + stock)
+        continue
+
+    ROC[stock] = round(((last_price - first_price) / first_price) * 100, 2)
+
+    temp = ta.rsi(stock_df.loc[stock, :].get("Adj Close"), length=3)
+    RSI[stock] = temp[finish]
+
 # print(ROC)
-print("start:", start, " finish: ", finish)
+# print("start:", start, " finish: ", finish)
 print("Highest Rate of Change% from start to finish:")
 output = sorted(ROC.items(), key=operator.itemgetter(1), reverse=True)
 # print(output)
 
 count = 0
 for i in output:
-    print(i[0], i[1], round(RSI[i[0]], 1), end=" ")
+    print(i[0], i[1], '%', round(RSI[i[0]], 1), end=" ")
     if RSI[i[0]] < 50:
         print("OK")
     else:
@@ -122,14 +163,3 @@ for i in output:
     count = count + 1
     if count >= 10:
         break
-
-#print(stock_df.loc["AAPL", :].get("Adj Close"))
-#RSI = {}
-#RSI = ta.rsi(stock_df.loc["AAPL", :].get("Adj Close"), length=5)
-#print(type(RSI))
-#print(RSI)
-#print(RSI[finish - timedelta(days=1)])
-
-#stock="AAPL"
-#print(stock_df.loc[stock, finish - timedelta(days=1)].get("Adj Close"))
-#print(stock_df.loc[stock, start].get("Adj Close"))
